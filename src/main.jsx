10a62ae13dad413c096cf75f2a48fddbab6bf215
Ryoma <roosensromarick@gmail.com>
2026-04-18 01:09:57 +0200

    add first features
    feat: add todo list functionality
    
    - Add todo on button click or Enter key
    - Delete todo on × click
    - Strike through text when task is checked
    -Show task count

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode, useState } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app.jsx";
 
-createRoot(document.getElementById('root')).render(
-
-   <div>
-      <h1>Welcome to my Todo-App</h1>
-    </div>
-
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
